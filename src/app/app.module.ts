fea6593477b585d0a1abe4e5b765d11698ff72e6
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-04-07 12:27:12 -0700

    create initial project and add team component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
